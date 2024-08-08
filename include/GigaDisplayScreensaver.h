be6eee2909450a62220ea73c2c83b8fa4c5dcc6e
denblock <dblock@stud.hs-offenburg.de>
2024-08-08 12:03:01 +0200

    1.0.2 New GigaDisplayBacklight version allows setting backlight to 0 (completely turning off), so now lvgl screen altering is needed anymore

--- include/GigaDisplayScreensaver.h
+++ include/GigaDisplayScreensaver.h
@@ -2,13 +2,12 @@
 
 #include <Arduino_GigaDisplay.h>
 #include <Arduino_GigaDisplayTouch.h>
-#include <lvgl.h>
 #include <mbed_shared_queues.h>
 
 class GigaDisplayScreensaver
 {
 public:
-    GigaDisplayScreensaver(Arduino_GigaDisplayTouch *touchDetector) : _backlight{GigaDisplayBacklight()}, _touchDetector{touchDetector} {}
+    GigaDisplayScreensaver(Arduino_GigaDisplayTouch* touchDetector) : _backlight{ GigaDisplayBacklight() }, _touchDetector{ touchDetector } {}
     void begin(uint64_t duration)
     {
         _backlight.begin();
@@ -17,10 +16,6 @@ public:
 
         _queue = mbed::mbed_event_queue();
         _queue->call_every(100ms, mbed::callback(this, &GigaDisplayScreensaver::tick));
-
-        _screensaverScreen = lv_obj_create(NULL);
-        lv_obj_clear_flag(_screensaverScreen, LV_OBJ_FLAG_SCROLLABLE);
-        lv_obj_set_style_bg_color(_screensaverScreen, lv_color_hex(0x000000), LV_PART_MAIN | LV_STATE_DEFAULT);
 
         _points = new GDTpoint_t[5];
     }
@@ -41,8 +36,6 @@ private:
         {
             if (_displayOn)
             {
-                _prevScreen = lv_disp_get_scr_act(NULL);
-                lv_disp_load_scr(_screensaverScreen);
                 _backlight.set(0);
 
                 _displayOn = false;
@@ -50,25 +43,20 @@ private:
         }
         else if (!_displayOn)
         {
-            lv_disp_load_scr(_prevScreen);
             _backlight.set(100);
-            _prevScreen = NULL;
 
             _displayOn = true;
         }
     }
 
     GigaDisplayBacklight _backlight;
-    Arduino_GigaDisplayTouch *_touchDetector;
+    Arduino_GigaDisplayTouch* _touchDetector;
 
     uint64_t _duration;
 
-    events::EventQueue *_queue;
+    events::EventQueue* _queue;
 
-    lv_obj_t *_screensaverScreen;
-    lv_obj_t *_prevScreen;
-
-    GDTpoint_t *_points;
+    GDTpoint_t* _points;
 
     uint64_t _lastTouchMillis = 0;
     bool _displayOn = true;
